6411ada0aab1615c4267ea459214e1cc5af05b13
Farid Murzone <faridmurzone@gmail.com>
2020-02-05 18:24:39 -0300

    MyCurrency also adds an event

--- 3_MyCurrency.sol
+++ 3_MyCurrency.sol
@@ -1,31 +1,21 @@
 pragma solidity >=0.5.0 <0.7.0;
 
 contract FaridCoin {
-    // The keyword "public" makes variables
-    // accessible from other contracts
     address public faridcoin;
     mapping (address => uint) public balances;
 
-    // Events allow clients to react to specific
-    // contract changes you declare
     event Sent(address from, address to, uint amount);
 
-    // Constructor code is only run when the contract
-    // is created
     constructor() public {
         faridcoin = msg.sender;
     }
 
-    // Sends an amount of newly created coins to an address
-    // Can only be called by the contract creator
     function farid(address receiver, uint amount) public {
         require(msg.sender == faridcoin);
         require(amount < 1e60);
         balances[receiver] += amount;
     }
 
-    // Sends an amount of existing coins
-    // from any caller to an address
     function send(address receiver, uint amount) public {
         require(amount <= balances[msg.sender], "Insufficient balance.");
         balances[msg.sender] -= amount;
